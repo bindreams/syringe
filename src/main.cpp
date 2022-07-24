e1b0f5f2fc66096c14c09142489a452f33ae7d14
Anna Zhukova <28830446+bindreams@users.noreply.github.com>
2022-07-24 15:56:55 +0300

    Fix printing and add exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include "syringe.hpp"
 
 int main(int argc, char** argv) {
-	fmt::print(syringe(argc, argv));
+	try {
+		fmt::print("{}", syringe(argc, argv));
+	} catch (std::exception& e) {
+		fmt::print(stderr, "{}", e.what());
+		return 1;
+	}
 }
